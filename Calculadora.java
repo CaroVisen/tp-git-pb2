93383c3238b15d60962dfe482faadf43d3c3b7db
juoliva95 <juoliva92@users.noreply.github.com>
2020-04-11 14:21:48 -0300

    Cambio del switch

--- Calculadora.java
+++ Calculadora.java
@@ -1,28 +1,33 @@
-import java.util.Scanner;
 public class Ahorcado {
- public static void main(String[] args) {
- Integer a;
- Integer b;
- Integer opcion;
- Integer resultado;
- do{
- System.out.println("Ingrese su operaciÃ³n /n 4 para sumar /n 3 para
-restar /n 2 para multiplicar /n 1 para dividir");
- opcion = teclado.next();
- }while(opcion < 1 && opcion > 4)
- System.out.println("Ingrese el primer numero");
- a = teclado.next();
- System.out.println("Ingrese el segundo numero");
- a = teclado.next();
- if(opcion.equals(4)){
- resultado = a + b;
- }else if(opcion.equals(3)){
- resultado = a - b;
- }else if(opcion.equals(2)){
- resultado = a * b;
- }else{
- resultado = a / b;
- }
- System.out.println("El resultado es: " + resultado.toString());
- }
+    public static void main(String[] args) {
+        Integer a;
+        Integer b;
+        Integer opcion;
+        Integer resultado;
+        do{
+            System.out.println("Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
+            opcion = teclado.next();
+        }while(opcion < 1 && opcion > 4)
+
+        System.out.println("Ingrese el primer numero");
+        a = teclado.next();
+        System.out.println("Ingrese el segundo numero");
+        a = teclado.next();
+
+        switch(opcion) {
+        	case 1:
+        		resultado = a + b;
+        		break;
+        	case 2:
+        		resultado = a - b;
+        		break;
+        	case 3:
+        		resultado = a * b;
+        		break;
+        	default:
+        		resultado = a / b;
+        }
+        
+        System.out.println("El resultado es " + resultado.toString());
+    }
 }
