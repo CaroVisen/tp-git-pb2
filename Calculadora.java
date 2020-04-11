19d47f60fd3daaa6c85f9d679be530180ffb7e46
unknown <carolinaaylen26@gmail.com>
2020-04-10 22:23:29 -0300

    Cambio de codigo

--- Calculadora.java
+++ Calculadora.java
@@ -6,19 +6,19 @@ public class Ahorcado {
  Integer opcion;
  Integer resultado;
  do{
- System.out.println("Ingrese su operación /n 1 para sumar /n 2 para
-restar /n 3 para multiplicar /n 4 para dividir");
+ System.out.println("Ingrese su operación /n 4 para sumar /n 3 para
+restar /n 2 para multiplicar /n 1 para dividir");
  opcion = teclado.next();
  }while(opcion < 1 && opcion > 4)
  System.out.println("Ingrese el primer numero");
  a = teclado.next();
  System.out.println("Ingrese el segundo numero");
  a = teclado.next();
- if(opcion.equals(1)){
+ if(opcion.equals(4)){
  resultado = a + b;
- }else if(opcion.equals(2)){
- resultado = a - b;
  }else if(opcion.equals(3)){
+ resultado = a - b;
+ }else if(opcion.equals(2)){
  resultado = a * b;
  }else{
  resultado = a / b;
