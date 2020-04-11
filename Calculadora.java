7fc24e72e50d8862ee210ccfc242c1f8fe7fdfe7
unknown <carolinaaylen26@gmail.com>
2020-04-11 19:44:24 -0300

    cambio del punto 8 con merge

--- Calculadora.java
+++ Calculadora.java
@@ -5,7 +5,7 @@ public class Ahorcado {
         Integer opcion;
         Integer resultado;
         do{
-            System.out.println("Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
+            System.out.println("Ingrese su operaciï¿½n /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
             opcion = teclado.next();
         }while(opcion < 1 && opcion > 4)
 
@@ -15,19 +15,25 @@ public class Ahorcado {
         a = teclado.next();
 
         switch(opcion) {
-        	case 1:
-        		resultado = a + b;
+        	case 1:{
+                    resultado = a + b;
+                    System.out.println("El resultado es de la suma es:" + resultado.toString());
+                }
         		break;
-        	case 2:
-        		resultado = a - b;
+        	case 2:{
+                    resultado = a - b;
+                    System.out.println("El resultado es de la resta es:" + resultado.toString());
+                }
         		break;
-        	case 3:
-        		resultado = a * b;
+        	case 3:{
+                    resultado = a * b;
+                    System.out.println("El resultado es de la multiplicacion es:" + resultado.toString());
+                }
         		break;
-        	default:
-        		resultado = a / b;
+        	default:{
+                    resultado = a / b;
+                    System.out.println("El resultado es de la divicion es:" + resultado.toString());
+                }
         }
-        
-        System.out.println("El resultado es " + resultado.toString());
     }
 }
